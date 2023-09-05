9ac9c2af91b148dd38bbcb618d3e68871f78eb4e
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-09-06 00:51:54 +0530

    Update sophia.py

--- sophia.py
+++ sophia.py
@@ -1,4 +1,4 @@
 from os import system # chala ja bhosdike madarchod
-system ("git clone https://github.com/AMBOT-v1/sophiamanager && cd sophiamanager && python3 -m MukeshRobot")
+system ("git clone https://github.com/AbhiMod/sophiamanager && cd sophiamanager && python3 -m MukeshRobot")
 # try af
 print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @AM_YTBOTT")
