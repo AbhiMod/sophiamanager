5f725f65b1fbc4d2cc4802da9aec2ed5bcf7b527
AMBOT-v1 <134328176+AMBOT-v1@users.noreply.github.com>
2023-09-05 10:09:15 +0530

    Update livegram.py

--- MukeshRobot/modules/livegram.py
+++ MukeshRobot/modules/livegram.py
@@ -1,8 +1,8 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-from Am import OWNER_ID
-from Am import pbot as bot
+from MukeshRobot import OWNER_ID
+from MukeshRobot import pbot as bot
 
 
 @bot.on_message(filters.private & filters.incoming)
