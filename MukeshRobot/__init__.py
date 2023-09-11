64950e50390e050fe1612b72dce7bd035d3fab1d
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-09-12 05:06:43 +0530

    Update __init__.py

--- MukeshRobot/__init__.py
+++ MukeshRobot/__init__.py
@@ -55,7 +55,7 @@ if ENV:
     STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", True))
     SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "+jLfuucjsi8kzMzE1")
     TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./")
-    TOKEN = os.environ.get("TOKEN", "6129232106:AAFnvrcgz5J9Ja-bW1H6Rtb4hE6oRF6aD6Q")
+    TOKEN = os.environ.get("TOKEN", "5998139371:AAHtiDIkaRkzr9rZ4gCHvGXv2mUbLOfhAMM")
     OPENAI_KEY = os.environ.get("OPENAI_KEY","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")
     TIME_API_KEY = os.environ.get("TIME_API_KEY", "9HK7J0H25AKQ")
     WORKERS = int(os.environ.get("WORKERS", 8))
