ee91c1dc6d6cbdc055670a0c5c632d9c3ea4ee20
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-09-24 10:35:44 +0530

    Update members_adding.py

--- MukeshRobot/modules/members_adding.py
+++ MukeshRobot/modules/members_adding.py
@@ -1,1 +1,101 @@
+import time
+from typing import List
 
+import requests
+from telegram import ParseMode, Update
+from telegram.ext import CallbackContext
+
+from MukeshRobot import StartTime, dispatcher
+from MukeshRobot.modules.disable import DisableAbleCommandHandler
+from MukeshRobot.modules.helper_funcs.chat_status import sudo_plus
+
+sites_list = {
+    "Telegram": "https://api.telegram.org",
+    "Kaizoku": "https://animekaizoku.com",
+    "Kayo": "https://animekayo.com",
+    "Jikan": "https://api.jikan.moe/v3",
+}
+
+
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+
+    return ping_time
+
+
+def ping_func(to_ping: List[str]) -> List[str]:
+    ping_result = []
+
+    for each_ping in to_ping:
+
+        start_time = time.time()
+        site_to_ping = sites_list[each_ping]
+        r = requests.get(site_to_ping)
+        end_time = time.time()
+        ping_time = str(round((end_time - start_time), 2)) + "s"
+
+        pinged_site = f"<b>{each_ping}</b>"
+
+        if each_ping == "Kaizoku" or each_ping == "Kayo":
+            pinged_site = f'<a href="{sites_list[each_ping]}">{each_ping}</a>'
+            ping_time = f"<code>{ping_time} (Status: {r.status_code})</code>"
+
+        ping_text = f"{pinged_site}: <code>{ping_time}</code>"
+        ping_result.append(ping_text)
+
+    return ping_result
+
+
+@sudo_plus
+def ping(update: Update, context: CallbackContext):
+    msg = update.effective_message
+
+    start_time = time.time()
+    message = msg.reply_text("ʏᴏᴜʀ ᴏᴅᴇʀ ᴄʜᴇᴄᴋɪɴɢ...")
+    end_time = time.time()
+    telegram_ping = str(round((end_time - start_time) * 1000, 3)) + " ms"
+    uptime = get_readable_time((time.time() - StartTime))
+
+    message.edit_text(
+       "#NEW_ODER\n\n"
+        "ʏᴏᴜʀ ᴍᴇᴍʙᴇʀꜱ\n"
+        "ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ ᴏᴅᴇʀ\n"
+        "ᴍᴇᴍʙᴇʀᴇ ᴀᴅᴅɪɴɢ ꜱᴛᴀʀᴛɪɴɢ ꜱᴏᴏɴ..\n"
+        "ᴛʜᴀɴᴋꜱ ꜰᴏʀ ᴏᴅᴇʀ 💕".format(telegram_ping, uptime),
+        parse_mode=ParseMode.HTML,
+    )
+
+
+
+
+
+PING_HANDLER = DisableAbleCommandHandler("add", ping, run_async=True)
+
+dispatcher.add_handler(PING_HANDLER)
+
+
+__command_list__ = ["add"]
+
+__handlers__ = [PING_HANDLER]
