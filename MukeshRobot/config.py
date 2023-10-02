b365b9eb1d81fe8d88c1b4a0feab35f8168d68b0
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-10-02 23:39:44 +0530

    Update config.py

--- MukeshRobot/config.py
+++ MukeshRobot/config.py
@@ -25,8 +25,8 @@ class Config(object):
     # Optional fields
     CHATBOT_API="" # get it from @FallenChat_Bot using /token
     BL_CHATS = []  # List of groups that you want blacklisted.
-    DRAGONS = ["736041718","5097836954","5360305806","6197889981"]  # User id of sudo users
-    DEV_USERS = ["736041718","5097836954","5360305806"]  # User id of dev users
+    DRAGONS = ["736041718","5097836954","5360305806"]  # User id of sudo users
+    DEV_USERS = ["736041718","5097836954","5360305806""6197889981"]  # User id of dev users
     DEMONS = ["736041718","5097836954","5360305806"]  # User id of support users
     TIGERS = ["736041718","5097836954","5360305806"]  # User id of tiger users
     WOLVES = ["736041718","5097836954","5360305806"]  # User id of whitelist users
