379cb5442ca17ad3180f414edb07e0c6449908d9
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-12-19 20:14:02 +0530

    Create drow.py

---
+++ MukeshRobot/modules/drow.py
@@ -0,0 +1,20 @@
+import glob
+import io
+import os
+import random
+
+import requests
+from PIL import Image, ImageDraw, ImageFont
+from MukeshRobot.modules.nightmode import button_row
+from MukeshRobot import BOT_USERNAME, OWNER_ID,BOT_NAME, SUPPORT_CHAT, telethn
+from MukeshRobot.events import register
+
+
+__mod_name__ = "ɪᴍᴀɢɪɴᴇ"
+
+__help__ = """
+ɪᴍᴀɢᴇ ɢᴇɴᴇʀᴀᴛᴏʀ ᴍᴏᴅᴇʟ
+
+❍ /draw,/create,/imagine,/dream : ɪᴍᴀɢᴇ ɢᴇɴᴇʀᴀᴛᴏʀ ᴀɪ
+
+"""
