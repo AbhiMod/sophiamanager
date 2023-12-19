a016914ecb07dfba1fa0978a11c8a2db43d59169
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-12-19 20:40:14 +0530

    Update Search.py

--- MukeshRobot/modules/Search.py
+++ MukeshRobot/modules/Search.py
@@ -10,11 +10,11 @@ from MukeshRobot import BOT_USERNAME, OWNER_ID,BOT_NAME, SUPPORT_CHAT, telethn
 from MukeshRobot.events import register
 
 
-__mod_name__ = "ɢᴏᴏɢʟᴇ-sᴇᴀʀᴄʜ"
-
 __help__ = """
 sᴇᴀʀᴄʜɪɴɢ ᴏɴ ɢᴏᴏɢʟᴇ
 
 ❍ /google : sᴇᴀʀᴄʜɪɴɢ ᴏɴ ɢᴏᴏɢʟᴇ...
 ❍ /stack : sᴇᴀʀᴄʜɪɴɢ ᴏɴ ɢᴏᴏɢʟᴇ...
 """
+
+__mod_name__ = "G-sᴇᴀʀᴄʜ"
