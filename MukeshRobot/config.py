473568a84e5e432263af23ba768ffc8071aa9d5e
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-12-19 20:19:31 +0530

    Update config.py

--- MukeshRobot/config.py
+++ MukeshRobot/config.py
@@ -6,7 +6,7 @@ class Config(object):
 
     API_ID = 12227067 # integer value, dont use ""
     API_HASH = "b463bedd791aa733ae2297e6520302fe"
-    TOKEN = "5998139371:AAGxG1ffSak2XJcLn1efFVuye7nqEN_X2BI"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    TOKEN = "5998139371:AAE9TXyrbJpODDPC95sICPBNtYYJxbR-WVk"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 5360305806 # If you dont know, run the bot and do /id in your private chat with it, also an integer
     
     SUPPORT_CHAT = "+eSTzpugepEMwNDBl"  # Your own group for support, do not add the @
