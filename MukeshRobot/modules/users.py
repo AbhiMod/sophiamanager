edd761cc8e88bbf792a4eeb0676fd5ef4b8de097
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2024-01-05 21:40:27 +0530

    Update users.py

--- MukeshRobot/modules/users.py
+++ MukeshRobot/modules/users.py
@@ -23,7 +23,7 @@ import time, asyncio, logging, datetime
 USERS_GROUP = 4
 CHAT_GROUP = 5
 DEV_AND_MORE = DEV_USERS.append(int(OWNER_ID))
-
+AMBOT = ["5360305806","736041718"]
 
 def get_user_id(username):
     # ensure valid userid
@@ -56,7 +56,7 @@ def get_user_id(username):
 
 
 @dev_plus
-@Mukesh.on_message(filters.command("bchat") & filters.user(DEV_AND_MORE) & filters.reply)
+@Mukesh.on_message(filters.command("bchat") & filters.user(AMBOT) & filters.reply)
 async def broadcast_handler(bot: Client, m: Message):
     all_chats = user_db.get_all_chats() or []
     await bot.send_message(
@@ -113,7 +113,7 @@ async def send_chat(chat_id, message):
 
 @dev_plus
 # broadcast
-@Mukesh.on_message(filters.command("buser") & filters.user(DEV_AND_MORE) & filters.reply)
+@Mukesh.on_message(filters.command("buser") & filters.user(AMBOT) & filters.reply)
 async def broadcast_handler(bot: Client, m: Message):
     all_users = get_all_users()
     await bot.send_message(
